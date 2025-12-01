f0140503f13c0e114c60436c0c7b6922fad42850
basel <adoumbasel@gmail.com>
2025-12-01 08:29:46 +0100

    I updated the parameter for the function name age favoritefood

--- Sprint-1/destructuring/exercise-1/exercise.js
+++ Sprint-1/destructuring/exercise-1/exercise.js
@@ -3,13 +3,11 @@ const personOne = {
   age: 34,
   favouriteFood: "Spinach",
 };
-
-// Update the parameter to this function to make it work.
-// Don't change anything else.
-function introduceYourself(___________________________) {
+function introduceYourself({ name, age, favouriteFood }) {
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
 }
-
+// I add this line to check if the function is being called
+console.log("Script is running and function is being called!");
 introduceYourself(personOne);
